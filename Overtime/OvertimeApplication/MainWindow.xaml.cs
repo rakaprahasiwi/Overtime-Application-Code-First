80588a9a69505be4da941f1e59ad51ea7ffea7c6
Raka Prahasiwi <prahasiwi11@gmail.com>
2019-06-12 01:06:16 +0700

    mockup
    new login and dashboard

--- Overtime/OvertimeApplication/MainWindow.xaml.cs
+++ Overtime/OvertimeApplication/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OvertimeApplication.UserInterface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,12 @@ namespace OvertimeApplication
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Dashboard dashboard = new Dashboard();
+            dashboard.ShowDialog();
         }
     }
 }
