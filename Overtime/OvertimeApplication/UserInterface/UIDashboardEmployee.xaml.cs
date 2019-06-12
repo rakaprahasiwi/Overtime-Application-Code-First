829aaa2fff383633aefcabf8df76ec01129ce708
Raka Prahasiwi <prahasiwi11@gmail.com>
2019-06-12 17:06:52 +0700

    mockup employee

---
+++ Overtime/OvertimeApplication/UserInterface/UIDashboardEmployee.xaml.cs
@@ -0,0 +1,65 @@
+﻿using OvertimeApplication.UserControlEmployee;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OvertimeApplication.UserInterface
+{
+    /// <summary>
+    /// Interaction logic for UIDashboardEmployee.xaml
+    /// </summary>
+    public partial class UIDashboardEmployee : Window
+    {
+        public UIDashboardEmployee()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+        }
+
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            GridMain.Children.Clear();
+
+            System.Windows.Controls.UserControl usc = null;
+            switch (((ListViewItem)((ListView)sender).SelectedItem).Name)
+            {
+                case "listViewItem":
+                    usc = new UCDashboardEmployee();
+                    GridMain.Children.Add(usc);
+                    break;
+                case "listViewItem1":
+                    usc = new UCCreateForm();
+                    GridMain.Children.Add(usc);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
